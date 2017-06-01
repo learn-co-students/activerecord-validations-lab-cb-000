9f87d6371de464f80d249fd8b8903fa653a94814
Aaron Kelton <aaron.k.kelton@gmail.com>
2017-06-01 21:07:50 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+
+  # validate in the model, as it defines how each instance ought to behave in the db
+  # name: validate: :presence = true
+
+  validates :name, presence: true , uniqueness: true
+  validates :phone_number, length: { minimum: 10 }
+
 end
