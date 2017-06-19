63e547e54f4e77eb4c996bab7a7a0063fed870c9
Tyler Bristol <tahbristol@gmail.com>
2017-06-19 03:26:46 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
-end
+      
+    
+  validates :title, presence: true, title: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+
+    end
+  
