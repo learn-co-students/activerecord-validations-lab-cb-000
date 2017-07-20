36c56025abb538382f1ccd49263618fd55118e2d
Eleanor Thomas <eleanor.thomas.res@gmail.com>
2017-07-20 01:08:08 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: {maximum: 250}
+  validates :content, length: {minimum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait?
+
+  def clickbait?
+    unless title =~ /Guess|Secret|Won't Believe|Top \d+/
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
