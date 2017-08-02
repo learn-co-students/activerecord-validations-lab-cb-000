9aa143780bf65f7691cb0627ea6a71a8f14b1971
youssefmys <mosa500@gmail.com>
2017-08-03 01:25:44 +0200

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title , :presence => true
+  validates :content, :length => {:minimum => 250}
+  validates :summary, :length => {:maximum => 250}
+  validates :category, :inclusion => {:in => %w{Fiction Non-Fiction}}
+
+  validate :validate_title
+
+  def validate_title
+    if self.title
+      if !self.title.match(/Won't Believe|Secret|Top[0-9]/)
+        errors.add(:title, "Invalid title content")
+      end
+    end
+  end
+
 end
