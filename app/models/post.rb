05726a643c4cad73cf1f16cb0a2c490dceb59c6f
Matt Hough <matt@hough.tv>
2017-12-05 08:03:34 +1000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+class ClickbaitValidator < ActiveModel::Validator
+    def validate(record)
+        if record.title
+            record.title.match(/Won't|Believe|Secret|Top \d*|Guess/) ? true : record.errors[:base] << "Not enough clickbait"
+        end
+    end
+end
+
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, Inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validates_with ClickbaitValidator
 end
