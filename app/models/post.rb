3b1d966cc70afb183e5412d2dbb77704f2ca0361
Michael Wahlin <wahlin@gmail.com>
2018-01-15 20:32:55 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,15 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 10 }
   validates :summary, length: {maximum: 299 }
   validates :category, inclusion: ["Fiction", "Non-Fiction"]
-  validates :title, exclusion: ["True Facts"]
+  validate :is_clickbait?
+
+  NOT_CLICKBAIT = [
+    /^True Facts/
+  ]
+
+  def is_clickbait?
+    if NOT_CLICKBAIT.any? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
