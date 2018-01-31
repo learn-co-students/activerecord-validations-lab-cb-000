20b38bb6b9ff72079d48aee6214b5503a49be055
Michael Anderson <michanders@msn.com>
2018-01-31 01:49:23 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, inclusion: {in: ["You Won't Believe These True Facts", "Secret", "Top
+[number]", "Guess"]}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
 end
