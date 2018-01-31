9b2bcb556000ee5301ca0ad3ddf3b058283b76d4
Heather Christman <efl7a@yahoo.de>
2018-01-31 18:38:32 -0500

    passing tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :non_clickbait
+
+  def non_clickbait
+    if self.title == nil
+      errors.add(:title, "Needs a title.")
+    else
+      clicky = ["won't", "believe", "secret", "top", "guess"]
+      title = self.title.split(" ")
+      if !title.any?{|word| clicky.include?(word.downcase)}
+        errors.add(:non_clickbait, "Title not clickbaitable")
+      end
+    end
+  end
 end
