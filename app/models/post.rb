d9c24650dd64e888eaf5a0b5c5297f9f55504746
Chris Cardle <cdcardle@gmail.com>
2018-05-31 14:50:40 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,11 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 50}
   validates :summary, length: {maximum: 50}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not Fiction or Non-Fiction."}
-  validates :title, clickbait: true
+  validate :is_clickbait?
+
+  def is_clickbait?
+    if title && title.match(/\b(Won't Believe|Guess|Secret|Top \d*)\b/i).blank?
+      errors.add(title, 'is not clickbait-y enough')
+    end
+  end
 end
--- app/validators/clickbait_validator.rb
+++
@@ -1,9 +0,0 @@
-class ClickbaitValidator < ActiveRecord::EachValidator
-  clickbait_regex = /\b(Won't Believe|Guess|Secret|Top \d*)\b/
-
-  def validate_each(record, attribute, value)
-    if value.match(clickbait_regex).blank?
-      record.errors.add(attribute, 'not clickbait-y enough')
-    end
-  end
-end
