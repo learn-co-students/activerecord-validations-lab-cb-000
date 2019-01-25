d5ecea9d60705e9d62e76be4055992242c275470
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-25 21:55:18 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+
+  Clickbait = [
+    /Top [0-9]*/i,
+    /Guess/i,
+    /Won't Believe/i,
+    /Secret/i
+  ]
+
+  def clickbait?
+    if Clickbait.none? { |phrase| phrase.match title}
+      errors.add(:title, "is not clickbait")
+    end
+  end
+
 end
