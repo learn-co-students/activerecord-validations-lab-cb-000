be6009449ee5f0f2938d3b5c2ea09bb34af04fdd
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-02-26 23:21:29 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,29 @@
+require 'pry'
 class Post < ActiveRecord::Base
   validates :title, presence: true
 
-
+  validate :must_be_clickbaity
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
 
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 
-  #
-  #
-  # validates :name, uniqueness: true
-  # validates :phone_number, uniqueness: true
+  def must_be_clickbaity
+
+    words = ["Won\'t Believe", "Secret", "Top [number]", "Guess"]
+    # binding.pry
+    if self.title == nil || !words.any? { |w| self.title.scan(w).length > 0 }
+      errors.add(:title, "Not baity enough bra")
+    end
+  end
+
+
 end
+
+
+
+#   def validate(record)
+#     binding.pry
+#     # words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+#     # if words.any? do |w| record.title.scan(w) } record.errors[:base] << "Not baity enough bra"
+#   end
