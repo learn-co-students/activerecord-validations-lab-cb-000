b3f53057e55c25517bed782a32b2073f1578c07f
kenkuts <kennethchoi19@gmail.com>
2019-06-01 04:56:09 -0400

    created custom error from validate custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,23 @@
 class Post < ActiveRecord::Base
-    include ActiveModel::Validations
-    validates_with PostValidator
+    
     validates :title, presence: true
-    # validates :content, length: { in: 0..500}
-    # validates :summary, length: { maximum: 250 }
-    # validates :category, inclusion: { in: %w( Fiction Non-Fiction )}
+    validates :content, length: { in: 250..500}
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w( Fiction Non-Fiction )}
+    validate :is_clickbait?
+
+    private
+
+    def is_clickbait?
+        click_bait = [
+          /Won't Believe/i,
+          /Secret/i,
+          /Top [0-9]*/i,
+          /Guess/i
+        ]
+
+        if click_bait.none? { |phrase| phrase.match title }
+            errors.add(:title, "must be clickbait!")
+        end
+    end
 end
